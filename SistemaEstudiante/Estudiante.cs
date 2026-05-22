6ff120b5a36c89181bfd2068d44b2680ce4e59b1
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-05-22 08:33:54 -0600

    SistemaEstudiante

---
+++ SistemaEstudiante/Estudiante.cs
@@ -0,0 +1,27 @@
+﻿public class Estudiante
+{
+    private string? _nombre;
+
+    public string? Nombre
+    {
+        get => _nombre;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException(
+                    "El nombre es obligatorio");
+            _nombre = value;
+        }
+    }
+
+    public Estudiante(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    public override string ToString()
+    {
+        return $"Nombre: {Nombre}";
+    }
+}
+
---
+++ SistemaEstudiante/Program.cs
@@ -0,0 +1,60 @@
+﻿var gestor = new GestorEstudiantes();
+
+bool salir = false;
+
+while (!salir)
+{
+    Console.Clear();
+    Console.WriteLine("=== GESTOR DE ESTUDIANTES ===");
+    Console.WriteLine("1. Agregar");
+    Console.WriteLine("2. Eliminar");
+    Console.WriteLine("3. Listar");
+    Console.WriteLine("4. Filtrar");
+    Console.WriteLine("5. Ordenar");
+    Console.WriteLine("6. Total");
+    Console.WriteLine("7. Salir");
+
+    Console.Write("\nOpción: ");
+    string opcion = Console.ReadLine()?.Trim() ?? string.Empty;
+
+    switch(opcion)
+    {
+        case "1":
+            Console.Write("\nNombre: ");
+            string nombre = Console.ReadLine()?.Trim() ?? string.Empty;
+            gestor.Agregar(nombre);
+            Console.ReadLine();
+            break;
+        case "2":
+            Console.Write("\nNombre: ");
+            string nombreEliminar = Console.ReadLine()?.Trim() ?? string.Empty;
+            gestor.Eliminar(nombreEliminar);
+            Console.ReadLine();
+            break;
+        case "3":
+            gestor.Listar();
+            Console.ReadLine();
+            break;
+        case "4":
+            Console.Write("\nTexto a buscar: ");
+            string texto = Console.ReadLine()?.Trim() ?? string.Empty;
+            gestor.Filtrar(texto);
+            Console.ReadLine();
+            break;
+        case "5":
+            gestor.OrdenarPorNombre();
+            Console.ReadLine();
+            break;
+        case "6":
+            gestor.MostrarTotal();
+            Console.ReadLine();
+            break;
+        case "7":
+            salir = true;
+            break;
+        default:
+            Console.WriteLine("Opción inválida.");
+            Console.ReadLine();
+            break;
+    }
+}
