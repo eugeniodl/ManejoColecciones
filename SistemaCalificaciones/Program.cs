24d60a8156cacb0fddfacc21f848a0cf909861ba
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-05-29 07:53:09 -0600

    SistemaCalificaciones

--- SistemaCalificaciones/Program.cs
+++ SistemaCalificaciones/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var registro = new RegistroEstudiantes();
+
+// Lista completa
+UIEstudiantes.MostrarLista("Todos los estudiantes", registro.ObtenerTodos());
+
+// Filtrar
+var aprobados = registro.Filtrar(e => e.EstaAprobado);
+UIEstudiantes.MostrarLista("Estudiantes aprobados", aprobados);
+
+// Ordenar
+var ordenados = registro.Ordenar(e => e.Nombre);
+UIEstudiantes.MostrarLista("Ordenados por nombre", ordenados);
+
+// Buscar
+var encontrado = registro.Buscar(e => e.NombreCompleto == "Ana García");
+Console.WriteLine("\n===== BÚSQUEDA =====");
+if (encontrado != null)
+    Console.WriteLine($"Encontrado: {encontrado}");
+else
+    Console.WriteLine("No encontrado");
+
+// Estadísticas
+UIEstudiantes.MostrarEstadisticas(registro);
--- SistemaCalificaciones/RegistroEstudiantes.cs
+++ SistemaCalificaciones/RegistroEstudiantes.cs
@@ -20,5 +20,36 @@
     {
         return _estudiantes.Where(criterio).ToList();
     }
+
+    public List<Estudiante> Ordenar(Func<Estudiante, object> criterio)
+    {
+        return _estudiantes.OrderBy(criterio).ToList();
+    }
+
+    public Estudiante? Buscar(Func<Estudiante, bool> criterio)
+    {
+        return _estudiantes.FirstOrDefault(criterio);
+    }
+
+    public double CalcularPromedio()
+    {
+        return Math.Round(_estudiantes.Average(e => e.Calificacion), 1);
+    }
+
+    public double ObtenerMaximaCalificacion()
+    {
+        return _estudiantes.Max(e => e.Calificacion);
+    }
+
+    public double ObtenerMinimaCalificacion()
+    {
+        return _estudiantes.Min(e => e.Calificacion);
+    }
+
+    public List<Estudiante> ObtenerTodos()
+    {
+        return new List<Estudiante>(_estudiantes);
+    }
+
 }
 
